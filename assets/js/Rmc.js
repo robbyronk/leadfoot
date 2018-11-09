cc5ee5cd263be51df02f00dc7a140fd441f00664
robbyronk <robby.ronk@gmail.com>
2018-11-09 19:03:31 +1300

    Move theme to it's own file

--- assets/js/Rmc.js
+++ assets/js/Rmc.js
@@ -2,30 +2,7 @@ import React, {Component} from 'react';
 import {ThemeProvider} from 'styled-components'
 import Participants from './Participants'
 import {Panel, PanelBody, PanelHeader, Tab, Tabs} from "./components";
-
-const theme = {
-    // can also look at colours with max brightness and saturation
-    // some colours inspired from rgbi palette
-    red: '#FF5555',
-    purple: '#c800ff',
-    green: '#55FF55',
-    blue: '#51cdef',
-    darkBlue: '#2b566c',
-    yellow: '#FFFF55',
-    orange: '#ffc03a',
-    grey: [
-        '#010101',
-        '#282828',
-        '#444',
-        '#666',
-        '#888',
-        '#bbb',
-        '#ddd',
-        '#fefefe',
-    ],
-    white: '#fefefe',
-    fontFamily: 'Fira Code, Sans-Serif;'
-}
+import theme from "./theme";
 
 class Rmc extends Component {
     render() {
