5667b64fb5f8b901d3b7dd6da1356f1ff2fc94c3
robbyronk <robby.ronk@gmail.com>
2018-11-09 19:00:32 +1300

    Move styled components to components folder, make nav tabs

--- assets/js/Rmc.js
+++ assets/js/Rmc.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import styled, {ThemeProvider} from 'styled-components'
-import path from 'lodash/fp/path';
+import {ThemeProvider} from 'styled-components'
 import Participants from './Participants'
+import {Panel, PanelBody, PanelHeader, Tab, Tabs} from "./components";
 
 const theme = {
     // can also look at colours with max brightness and saturation
@@ -23,60 +23,9 @@ const theme = {
         '#ddd',
         '#fefefe',
     ],
+    white: '#fefefe',
     fontFamily: 'Fira Code, Sans-Serif;'
 }
-
-const Button = styled.button`
-  background: ${path('theme.blue')};
-  border-radius: 3px;
-  border: 2px solid ${path('theme.blue')};
-  color: ${path('theme.grey[0]')};
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`
-
-const PurpleButton = styled(Button)`
-  background: ${path('theme.purple')};
-  border: 2px solid ${path('theme.purple')};
-  color: ${path('theme.grey[0]')};
-`
-
-const YellowButton = styled(Button)`
-  background: ${path('theme.yellow')};
-  border: 2px solid ${path('theme.yellow')};
-  color: ${path('theme.grey[0]')};
-`
-
-const Panel = styled.div`
-  min-height: 100vh;
-  background-color: ${path('theme.grey[0]')};
-  display: grid;
-  border: 5px solid ${path('theme.blue')};
-  grid-template-rows: 5em auto;
-  font-family: ${path('theme.fontFamily')};
-`
-
-const PanelHeader = styled.div`
-  background-color: ${path('theme.darkBlue')};
-  color: ${path('theme.grey[7]')};
-  padding: 0.5em 1em;
-  border-bottom: 5px solid ${path('theme.blue')};
-`
-
-const PanelBody = styled.div`
-  color: ${path('theme.grey[7]')};
-`
-
-const ParticipantsGrid = styled.div`
-  padding: 1em;
-  display: grid;
-  grid-template-columns: 3em repeat(auto-fit, minmax(100px, 1fr));
-`
-
-const YellowText = styled.div`
-  color: ${path('theme.yellow')};
-`
-
 
 class Rmc extends Component {
     render() {
@@ -88,6 +37,23 @@ class Rmc extends Component {
                             |>RACE CONTROL
                         </h2>
                     </PanelHeader>
+                    <Tabs>
+                        <Tab active>
+                            Session
+                        </Tab>
+                        <Tab>
+                            Timing
+                        </Tab>
+                        <Tab>
+                            Map
+                        </Tab>
+                        <Tab>
+                            Vehicle
+                        </Tab>
+                        <Tab>
+                            Laps
+                        </Tab>
+                    </Tabs>
                     <PanelBody>
                         <Participants/>
                     </PanelBody>
---
+++ assets/js/components/index.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import path from "lodash/fp/path";
+
+export const Button = styled.button`
+  background: ${path('theme.blue')};
+  border-radius: 3px;
+  border: 2px solid ${path('theme.blue')};
+  color: ${path('theme.grey[0]')};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+export const PurpleButton = styled(Button)`
+  background: ${path('theme.purple')};
+  border: 2px solid ${path('theme.purple')};
+  color: ${path('theme.grey[0]')};
+`
+
+export const YellowButton = styled(Button)`
+  background: ${path('theme.yellow')};
+  border: 2px solid ${path('theme.yellow')};
+  color: ${path('theme.grey[0]')};
+`
+
+export const Panel = styled.div`
+  min-height: 100vh;
+  background-color: ${path('theme.grey[0]')};
+  display: grid;
+  border: 5px solid ${path('theme.blue')};
+  grid-template-rows: 5em 5em auto;
+  font-family: ${path('theme.fontFamily')};
+`
+
+export const PanelHeader = styled.div`
+  background-color: ${path('theme.darkBlue')};
+  color: ${path('theme.grey[7]')};
+  padding: 0.5em 1em;
+  border-bottom: 5px solid ${path('theme.blue')};
+`
+
+export const PanelBody = styled.div`
+  color: ${path('theme.grey[7]')};
+`
+
+export const ParticipantsGrid = styled.div`
+  padding: 1em;
+  display: grid;
+  grid-template-columns: 3em repeat(auto-fit, minmax(100px, 1fr));
+`
+
+export const YellowText = styled.div`
+  color: ${path('theme.yellow')};
+`
+
+export const Tabs = styled.div`
+  padding: 0;
+  margin: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  border-bottom: 5px solid ${path('theme.blue')};
+`
+
+export const Tab = styled.div`
+  background-color: ${props => props.active ? props.theme.darkBlue : 'transparent'};
+  color: ${path('theme.white')};
+  padding: 1em;
+  &:not(:last-child) {
+    border-right: 5px solid ${path('theme.blue')};
+  }
+  text-align: center;
+`
