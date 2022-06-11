574f083d04be87566a54563cd7d0917ef69a8f41
Robby Ronk <robby.ronk@gmail.com>
2022-06-11 15:10:52 -0400

    Add math and circuits library

--- mix.exs
+++ mix.exs
@@ -48,6 +48,8 @@ defmodule RaceControl.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
+      {:circuits_uart, "~> 1.4"},
+      {:math, "~> 0.7.0"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
