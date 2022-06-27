4aa6ba417e0dd66d86d802cd8f00cca0016868a8
Robby Ronk <robby.ronk@gmail.com>
2022-06-26 21:55:18 -0400

    Add a method to record a file of raw packets

--- lib/leadfoot/read_udp.ex
+++ lib/leadfoot/read_udp.ex
@@ -5,12 +5,16 @@ defmodule Leadfoot.ReadUdp do
   alias Leadfoot.ParsePacket
   alias Phoenix.PubSub
 
+  @initial_state %{
+    file: nil
+  }
+
   def start_link(state \\ %{}, opts \\ []) do
     GenServer.start_link(__MODULE__, state, opts)
   end
 
   def init(_opts) do
-    {:ok, %{}, {:continue, :open_port}}
+    {:ok, @initial_state, {:continue, :open_port}}
   end
 
   def handle_continue(:open_port, state) do
@@ -22,6 +26,31 @@ defmodule Leadfoot.ReadUdp do
   def handle_info({:udp, _socket, _ip, _port, data}, state) do
     event = ParsePacket.parse_packet(data)
     PubSub.broadcast(Leadfoot.PubSub, "session", {:event, event})
+
+    if state.file != nil do
+      len = byte_size(data)
+      IO.binwrite(state.file, <<len::16>> <> data)
+    end
+
     {:noreply, state}
+  end
+
+  def start(pid, filename) do
+    GenServer.cast(pid, {:start, filename})
+  end
+
+  def stop(pid) do
+    GenServer.cast(pid, :stop)
+  end
+
+  def handle_cast({:start, filename}, state) do
+    {:ok, file} = File.open(filename, [:write])
+
+    {:noreply, %{state | file: file}}
+  end
+
+  def handle_cast(:stop, state) do
+    File.close(state.file)
+    {:noreply, @initial_state}
   end
 end
