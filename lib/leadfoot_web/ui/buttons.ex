d90ba6f7b060802ef66ada25709aadd78113b3f3
Robby Ronk <robby.ronk@gmail.com>
2022-09-07 23:37:40 -0400

    combine assign_new

--- lib/leadfoot_web/ui/buttons.ex
+++ lib/leadfoot_web/ui/buttons.ex
@@ -2,8 +2,10 @@ defmodule LeadfootWeb.UI.Buttons do
   use Phoenix.Component
 
   def btn(assigns) do
-    assigns = assign_new(assigns, :class, fn -> "" end)
-    assigns = assign_new(assigns, :type, fn -> "button" end)
+    assigns =
+      assigns
+      |> assign_new(:class, fn -> "" end)
+      |> assign_new(:type, fn -> "button" end)
 
     class =
       "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded #{assigns.class}"
