b4e0789054332d41096f705366aef79303f8153a
Robby Ronk <robby.ronk@gmail.com>
2023-05-06 00:29:17 -0400

    Add restart command

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,12 @@
 alias Leadfoot.GearRatios
 alias Leadfoot.ReadFile
 alias Leadfoot.ReadUdp
+alias Leadfoot.Session.Session
+
+defmodule App do
+  def restart do
+    Application.stop(:leadfoot)
+    recompile()
+    Application.ensure_all_started(:leadfoot)
+  end
+end
