3befc6f76ba506ddaea3f35ff4f4fbee8081843f
Robby Ronk <robby.ronk@gmail.com>
2023-10-04 01:53:45 -0400

    sort deps

--- mix.exs
+++ mix.exs
@@ -33,28 +33,28 @@ defmodule Leadfoot.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.2"},
-      {:phoenix_ecto, "~> 4.4"},
+      {:circuits_uart, "~> 1.4"},
+      {:contex, "~> 0.4.0"},
       {:ecto_sql, "~> 3.10.1"},
       {:ecto_sqlite3, "~> 0.10.0"},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.18"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
+      {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:circuits_uart, "~> 1.4"},
       {:math, "~> 0.7.0"},
-      {:contex, "~> 0.4.0"},
+      {:phoenix, "~> 1.7.2"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 3.3"},
+      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.18.18"},
+      {:plug_cowboy, "~> 2.5"},
       {:postgrex, "~> 0.17.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:swoosh, "~> 1.3"},
+      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
